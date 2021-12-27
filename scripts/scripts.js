39f14e9d892cd0b713f9d4deac17921d6e73b5e3
Blain Crawford <blaincrawford@gmail.com>
2021-12-27 16:58:10 -0500

    have basic working functionality need to clean up and expand

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,40 @@
-let body = document.querySelector('body');
-let h1 = document.createElement('h1');
-h1.textContent =  'Hello, World!';
-body.appendChild(h1);
+//creating elements for DOM manipulation
+const slider = document.querySelector('#grid-maker');
+const label = document.querySelector('#label');
+const etchScreen = document.querySelector('#screen');
+const blackButton = document.querySelector('#black');
+const fibers = document.getElementsByClassName('block');
+
+
+//gets rid of grid
+const deleteGrid = function(){
+    while(etchScreen.firstChild){
+      etchScreen.removeChild(etchScreen.firstChild);
+  }
+}
+//creates grid in etch a sketch
+const gridMaker = function() {
+  deleteGrid();
+  let gridNumber = this.value;
+  label.textContent = gridNumber;
+  etchScreen.style.cssText = `grid-template-columns: repeat(${gridNumber}, 1fr); grid-template-rows: repeat(${gridNumber}, 1fr);`
+
+  for(let i = 0; i < gridNumber**2; i++){
+    let newDiv = document.createElement('div');
+    newDiv.classList = 'block'
+    newDiv.addEventListener('mouseover', black);
+    etchScreen.appendChild(newDiv);
+  }
+};
+
+slider.addEventListener('input', gridMaker);
+
+//color divs in etch-a-sketch
+const black = function() {
+  this.style.cssText = 'background-color: black;';
+};
+
+
+
+
+
